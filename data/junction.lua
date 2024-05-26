e05b4b7d883176b358230fd16283657d5af691ac
OddColouredBanana <oddcolouredbanana@gmail.com>
2024-05-26 12:51:13 -1000

    Reset2

--- data/junction.lua
+++
@@ -1,4 +0,0 @@
--- The purpose of this file is just to allow us to refer to tuning.lua from both IC and hacked combotest;
--- hence, combotest's operation relies on a symbolic link being set up in ImpossibleCreatures/data called
--- "junction.lua" which links to tellurian/data/tuning.lua.
-dofilepath("data:sigma/tuning.lua")
